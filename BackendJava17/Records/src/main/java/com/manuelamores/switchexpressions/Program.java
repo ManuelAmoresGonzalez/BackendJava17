28b89f3dd2f1a3898c46e723bafad415dfaf7f1d
Manuel Enrique Amores Gonzalez <manuel.amores@globant.com>
2023-11-07 12:06:18 -0600

    Changes

--- BackendJava17/Records/src/main/java/com/manuelamores/switchexpressions/Program.java
+++ BackendJava17/Records/src/main/java/com/manuelamores/switchexpressions/Program.java
@@ -7,6 +7,38 @@ public class Program {
 
         program.m1Basic("A");
         program.m2Arrow("B");
+        String result = program.m3Yield("C");
+        System.out.println(result);
+        program.m4ArrowReturn("B");
+        program.m5MultipleCase("A");
+    }
+
+    private void m5MultipleCase(String param) {
+        switch (param){
+            case "A", "B" -> System.out.println("Caso a y b");
+            case "C", "D" -> System.out.println("Caso c y d");
+            default -> System.out.println("Ningun otro");
+        }
+    }
+
+    private String m4ArrowReturn(String param) {
+        return switch (param){
+            case "A" -> "Case A";
+            case "B" -> {
+                System.out.println("Case B");
+                yield "Case B";
+            }
+            default -> "Default case";
+        };
+    }
+
+    private String m3Yield(String param) {
+
+        return switch (param){
+            case "A": yield "Case A";
+            case "B": yield "Case B";
+            default:  yield "Default case";
+        };
     }
 
     private void m2Arrow(String param) {
