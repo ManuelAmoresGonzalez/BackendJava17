f3865ffc841fff7c37a789be5f88829ec1b2c95a
Manuel Enrique Amores Gonzalez <manuel.amores@globant.com>
2023-11-15 16:10:55 -0600

    Creating dtocontroller

--- BackendJava17/projectspringboot/manuel-sales/src/main/java/com/manuelamores/controller/CategoryController.java
+++ BackendJava17/projectspringboot/manuel-sales/src/main/java/com/manuelamores/controller/CategoryController.java
@@ -25,7 +25,7 @@ public class CategoryController{
     public ResponseEntity<List<CategoryDTO>>  readAll() throws Exception {
         List<CategoryDTO> categoryDTO =  service.readAll()
                 .stream()
-                .map( value -> mapper.map(value, CategoryDTO.class))
+                .map(this::convertToDTO)
                 .toList();
         return new ResponseEntity<>(categoryDTO, HttpStatus.OK);
     }
@@ -33,25 +33,33 @@ public class CategoryController{
     @GetMapping(path = "/{id}")
     public ResponseEntity<CategoryDTO> readById(@PathVariable ("id") Integer id) throws Exception {
          Category category = service.readById(id);
-         CategoryDTO dto = mapper.map(category, CategoryDTO.class);
-         return new ResponseEntity<>(dto, HttpStatus.OK);
+         return new ResponseEntity<>(convertToDTO(category), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Category> create(@RequestBody Category category) throws Exception {
-        Category createdCategory = service.save(category);
-        return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
+    public ResponseEntity<CategoryDTO> create(@RequestBody CategoryDTO dto) throws Exception {
+        Category createdCategory = service.save(convertToEntity(dto));
+        return new ResponseEntity<>(convertToDTO(createdCategory), HttpStatus.CREATED);
     }
 
     @PutMapping(path = "/{id}")
-    public ResponseEntity<Category> update(@RequestBody Category category, @PathVariable ("id") Integer id) throws Exception {
-        Category updatedCategory = service.update(category, id);
-        return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
+    public ResponseEntity<CategoryDTO> update(@RequestBody CategoryDTO dto, @PathVariable ("id") Integer id) throws Exception {
+        Category updatedCategory = service.update(convertToEntity(dto), id);
+        return new ResponseEntity<>(convertToDTO(updatedCategory), HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable ("id") Integer id) throws Exception {
         service.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private CategoryDTO convertToDTO(Category category) {
+        return mapper.map(category, CategoryDTO.class);
+    }
+
+
+    private Category convertToEntity(CategoryDTO dto) {
+        return mapper.map(dto, Category.class);
     }
 }
